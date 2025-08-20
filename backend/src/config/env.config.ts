616a32b0b6dc22cd74f86807c908822b00c63776
edwardbudaza <56827199+edwardbudaza@users.noreply.github.com>
2025-08-20 10:24:07 +0000

    backend configurations

---
+++ backend/src/config/env.config.ts
@@ -0,0 +1,15 @@
+import { getEnv } from 'src/utils/get-env';
+
+const envConfig = () => ({
+  NODE_ENV: getEnv('NODE_ENV', 'development'),
+  PORT: getEnv('PORT', '3000'),
+  BASE_PATH: getEnv('BASE_PATH', '/api'),
+  MONGO_URI: getEnv('MONGO_URI', ''),
+
+  JWT_SECRET: getEnv('JWT_SECRET', 'secret_jwt'),
+  JWT_EXPIRES_IN: getEnv('JWT_EXPIRES_IN', '1d'),
+  LOG_LEVEL: getEnv('LOG_LEVEL', 'info'),
+  ALLOWED_ORIGINS: getEnv('ALLOWED_ORIGINS'),
+});
+
+export const Env = envConfig();
---
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,28 @@
+import { ErrorRequestHandler, Response } from 'express';
+import { HTTPSTATUS } from 'src/config/http.config';
+
+export const errorHandler: ErrorRequestHandler = (error, req, res, next): any => {
+  console.log(`Error occured on PATH: ${req.path}`, {
+    body: req.body,
+    params: req.params,
+    error,
+  });
+
+  if (error instanceof SyntaxError && 'body' in error) {
+    return res.status(HTTPSTATUS.BAD_REQUEST).json({
+      message: 'Invalid json format, please check your request body',
+    });
+  }
+
+  //   if (error instanceof AppError) {
+  //     return res.status(error.statusCode).json({
+  //       message: error.message,
+  //       errorCode: error.errorCode,
+  //     });
+  //   }
+
+  return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
+    message: 'Internal Server error',
+    error: error?.message || 'Unknown error',
+  });
+};
